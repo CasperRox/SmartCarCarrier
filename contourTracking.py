69e27608f714d8ab2a57c1c76cc2d352e634067d
DamindaB <daminda.12@cse.mrt.ac.lk>
2017-01-27 13:11:35 +0530

    initial commits

--- contourTracking.py
+++ contourTracking.py
@@ -1,17 +1,16 @@
 import numpy as np
 import cv2
-import sys
+import time
 
 # cam = cv2.VideoCapture(0)
-cam = cv2.VideoCapture("/home/rangathara/FYP/RoadDetection/CutHighwayVideo.mp4")
+# cam = cv2.VideoCapture("/home/rangathara/FYP/RoadDetection/CutHighwayVideo.mp4")
 
 while (True):
-	s, img = cam.read()
-	# img = cv2.imread("/home/rangathara/FYP/images/ADS_1086.JPG")
+	# s, img = cam.read()
+	img = cv2.imread("/home/rangathara/FYP/images/colombo---kandy-road-warakapola.jpg")
 	imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	ret,thresh = cv2.threshold(imgGray,35,255,cv2.THRESH_BINARY)
 	edges = cv2.Canny(imgGray, 30, 150)
-	# edges = cv2.Canny(thresh, 30, 150)
 
 	kernel = np.ones((5,5),np.uint8)
 	closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
@@ -21,22 +20,35 @@ while (True):
 
 	# im2,contours,hierarchy = cv2.findContours(edges,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 	im2,contours,hierarchy = cv2.findContours(thresh1,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-	# im2,contours,hierarchy = cv2.findContours(thresh, 1, 2)
-	# im2,contours,hierarchy = cv2.findContours(edges, 1, 2)
-	contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+	contours = sorted(contours, key=cv2.contourArea, reverse=True)[:3]
 
-	for cnt in contours[1:4]:
-	# cnt = contours[0]
+	orb = cv2.ORB_create()
+    # find the keypoints and descriptors with SIFT
+	kpOrig, desOrig = orb.detectAndCompute(imgGray,None)
+    # create BFMatcher object
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING2, crossCheck=True)
+
+	for cnt in contours[1:3]:	# the biggest rectangle is around the whole image
 		x,y,w,h = cv2.boundingRect(cnt)
+		imgCrop = imgGray[y:y+h, x:x+w]
+		kpObj, desObj = orb.detectAndCompute(imgCrop,None)
+		if len(kpObj) == 0:
+			continue
+	    # Match descriptors.
+		matches = bf.match(desOrig,desObj)
+		matches = sorted(matches, key = lambda x:x.distance)
+		img3 = cv2.drawMatches(imgGray,kpOrig,imgCrop,kpObj,matches[:10],None, flags=2)
+		cv2.imshow("cropped", img3)
 		cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
-	cv2.imshow("morphology", thresh1)
 	cv2.imshow("threshold", thresh)
 	cv2.imshow("edge", edges)
 	cv2.imshow("result", img)
 
 	if cv2.waitKey(10) & 0xff == ord('q'):
 		break
+
+	# time.sleep(0.25)
 
 # cv2.waitKey(0)
 cam.release()
