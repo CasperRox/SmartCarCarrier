78b90d51ce723dcbb4fdfc6ebbe2a3d40b74d8d8
DamindaB <daminda.12@cse.mrt.ac.lk>
2017-02-07 12:02:44 +0530

    initial commits

--- contourTracking.py
+++ contourTracking.py
@@ -10,15 +10,17 @@ def detectSameObject(imgSrc, imgTarget):
 	# scaling = 0
 	# xyCordinates = (0,0)
 	while True:
-		if (imgSrc.shape[0]<imgTarget.shape[0]) or (imgSrc.shape[1]<imgTarget.shape[1]):
+		if imgSrc.shape[0]<imgTarget.shape[0] or imgSrc.shape[1]<imgTarget.shape[1]:
 			break
 		# imgDiff = np.full((imgTarget.shape[0],imgTarget.shape[1]), 255, dtype=int)
-		for y in (0, imgSrc.shape[0]-imgTarget.shape[0], 10):
-			for x in (0, imgSrc.shape[1]-imgTarget.shape[1], 10):
+		for y in (0, imgSrc.shape[0]-imgTarget.shape[0], 1):
+			for x in (0, imgSrc.shape[1]-imgTarget.shape[1], 1):
 				# print (y)
 				imgTemp = imgSrc[y:y+imgTarget.shape[0], x:x+imgTarget.shape[1]]
-				imgDiff = imgTemp - imgTarget
-				# imgDiff = cv2.subtract(imgTemp, imgTarget)
+				if not np.array_equal(imgTarget.shape,imgTemp.shape):
+					continue
+				# imgDiff = imgTemp - imgTarget
+				imgDiff = cv2.absdiff(imgTemp, imgTarget)	# per-element absolute difference
 				zeroCount = (imgDiff==0).sum()	# returns the number of zeros in the array
 				if zeroCount > zeroCountMax:	# get the most similar comparison
 					zeroCountMax = zeroCount
@@ -30,13 +32,15 @@ def detectSameObject(imgSrc, imgTarget):
 		w = int(imgSrc.shape[1]/1.5)
 		imgSrc = imutils.resize(imgSrc, width=w)
 		sizeCounter *= 1.5
+	# print (zeroCountMax)
 	xy = [int(x*scaling) for x in xyCordinates]
-	cv2.imshow("Detected", imgOrig[xy[0]:xy[0]+imgTarget.shape[0], xy[1]:xy[1]+imgTarget.shape[1]])
+	cv2.rectangle(img,(xy[1],xy[0]),(xy[1]+imgTarget.shape[1],xy[0]+imgTarget.shape[0]),(0,0,255),2)
+	# cv2.imshow("Detected", imgOrig[xy[0]:xy[0]+imgTarget.shape[0], xy[1]:xy[1]+imgTarget.shape[1]])
 	return scaling
 
 
 # cam = cv2.VideoCapture(0)
-cam = cv2.VideoCapture("/home/rangathara/FYP/RoadDetection/CutHighwayVideo.mp4")
+cam = cv2.VideoCapture("/home/rangathara/FYP/RoadDetection/CutHighwayVideo2.mp4")
 
 while (True):
 	s, img = cam.read()
@@ -46,22 +50,25 @@ while (True):
 	edges = cv2.Canny(imgGray, 30, 150)
 
 	kernel = np.ones((5,5),np.uint8)
-	closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-	opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
-	ret,thresh1 = cv2.threshold(opening,35,255,cv2.THRESH_BINARY_INV)
+	closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)	# dilation then erosion
+	# cv2.imshow("morphology3", closing)
+	# opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)	# erosion then dilation
+	# cv2.imshow("morphology2", opening)
+	ret,thresh1 = cv2.threshold(closing,127,255,cv2.THRESH_BINARY_INV)
 	# cv2.imshow("morphology1", thresh1)
 
 	# im2,contours,hierarchy = cv2.findContours(edges,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 	im2,contours,hierarchy = cv2.findContours(thresh1,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-	contours = sorted(contours, key=cv2.contourArea, reverse=True)[:3]
+	contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
 
 	# orb = cv2.ORB_create()
 	# kpOrig, desOrig = orb.detectAndCompute(imgGray,None)
 	# bf = cv2.BFMatcher(cv2.NORM_HAMMING2, crossCheck=True)
 
-	for cnt in contours[1:3]:	# the biggest rectangle is around the whole image
+	for cnt in contours[1:5]:	# the biggest rectangle is around the whole image
 		x,y,w,h = cv2.boundingRect(cnt)
-		imgCrop = imgGray[y:y+h, x:x+w]
+		# imgCrop = imgGray[y:y+h, x:x+w]
+		imgCrop = closing[y:y+h, x:x+w]
 		# kpObj, desObj = orb.detectAndCompute(imgCrop,None)
 		# if len(kpObj) == 0:
 			# continue
@@ -69,9 +76,10 @@ while (True):
 		# matches = sorted(matches, key = lambda x:x.distance)
 		# img3 = cv2.drawMatches(imgGray,kpOrig,imgCrop,kpObj,matches[:10],None, flags=2)
 		# cv2.imshow("cropped", img3)
-		cv2.imshow("cropped", imgCrop)
+		# cv2.imshow("cropped", imgCrop)
 		cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-		scaling = detectSameObject(imgGray, imgCrop)
+		# scaling = detectSameObject(imgGray, imgCrop)
+		scaling = detectSameObject(closing, imgCrop)
 		# print (scaling)
 		# time.sleep(1)
 
@@ -82,7 +90,7 @@ while (True):
 	if cv2.waitKey(10) & 0xff == ord('q'):
 		break
 
-	# time.sleep(0.25)
+	time.sleep(0.5)
 
 # cv2.waitKey(0)
 cam.release()
